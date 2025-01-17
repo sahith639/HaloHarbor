960a96b293a491e7c874450a254483be1339deaa
Sahith-Reddy-J <144086929+Sahith-Reddy-J@users.noreply.github.com>
2025-01-16 20:55:09 -0500

    Revert "sideBar UI"
    This reverts commit 68c43b02db4065c78c747d8b1f8ee2ae7a3e3702.

--- user/frontend-user/src/components/SideBar.jsx
+++ user/frontend-user/src/components/SideBar.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Sidebar, MenuItem, Menu} from "react-pro-sidebar";
+import { Sidebar, Menu, MenuItem} from "react-pro-sidebar";
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import { NavLink } from "react-router-dom";
 // import { tokens } from "../theme";
@@ -17,8 +17,7 @@ import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutline
 import LocalFloristOutlinedIcon from '@mui/icons-material/LocalFloristOutlined';
 import QuestionMarkOutlinedIcon from '@mui/icons-material/QuestionMarkOutlined';
 import DocumentScannerOutlinedIcon from '@mui/icons-material/DocumentScannerOutlined';
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
-import '../menu.css';
+
 // import StressScore from "../StressScore";
 // import Logout from "../../components/Logout";
 
@@ -78,41 +77,15 @@ const ThisProSidebar = () => {
 
   return (
     // https://github.com/azouaoui-med/react-pro-sidebar#readme
-    <div style={{"background": "#272C34","min-height": "100vh",  "border":'#272C34'}}>
-    <Sidebar collapsed={isCollapsed} style={{"background": "#272C34","min-height": "100vh",  "border":'#272C34'}}>
-      {/* <SidebarHeader/> */}
-      <div className="home_header" style={{"background": "#272C34"}}>
-        <div className="header_title" style={{"padding-left": "0%","width": "100%"}}>
-          <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clip-path="url(#clip0_23_17)">
-              <path d="M11.6641 28.3333H13.4807C13.9474 25.55 15.5474 23.1666 17.8141 21.6666H11.6641L11.6641 28.3333Z" fill="#4D73EF"/>
-              <path d="M8.33333 31.6667L8.33333 8.33333L31.6667 8.33333L31.6667 20H33.3333C33.9 20 34.45 20.0667 35 20.15V8.33333C35 6.5 33.5 5 31.6667 5L8.33333 5C6.5 5 5 6.5 5 8.33333L5 31.6667C5 33.5 6.5 35 8.33333 35H14.6833C14.1 33.9833 13.6833 32.8667 13.4833 31.6667L8.33333 31.6667Z" fill="#4D73EF"/>
-              <path d="M18.3307 11.6666L11.6641 11.6666L11.6641 18.3333H18.3307V11.6666Z" fill="#4D73EF"/>
-              <path d="M28.3307 11.6666L21.6641 11.6666V18.3333H28.3307V11.6666Z" fill="#4D73EF"/>
-              <path d="M26.6641 33.3334H23.3307C21.4974 33.3334 19.9974 31.8334 19.9974 30C19.9974 28.1667 21.4974 26.6667 23.3307 26.6667L26.6641 26.6667V23.3334L23.3307 23.3334C19.6474 23.3334 16.6641 26.3167 16.6641 30C16.6641 33.6834 19.6474 36.6667 23.3307 36.6667H26.6641L26.6641 33.3334Z" fill="#4D73EF"/>
-              <path d="M33.3333 23.3334H30V26.6667H33.3333C35.1667 26.6667 36.6667 28.1667 36.6667 30C36.6667 31.8334 35.1667 33.3334 33.3333 33.3334H30V36.6667H33.3333C37.0167 36.6667 40 33.6834 40 30C40 26.3167 37.0167 23.3334 33.3333 23.3334Z" fill="#4D73EF"/>
-              <path d="M33.3359 31.6667V28.3334L28.3359 28.3334H23.3359V31.6667L31.6693 31.6667H33.3359Z" fill="#4D73EF"/>
-            </g>
-            <defs>
-              <clipPath id="clip0_23_17">
-              <rect width="40" height="40" fill="white"/>
-              </clipPath>
-            </defs>
-          </svg>
-          <h3>Halo Harbor</h3>
-        </div>
-        </div>
-      <Menu style={{"background": "#272C34","min-height": "100vh","color":"#a7a9ac", "border":'#272C34'}} menuItemStyles={{
+    <Sidebar collapsed={isCollapsed} >
+      <SidebarHeader/>
+      <Menu iconShape="square" menuItemStyles={{
       button: {
         // the active class will be added automatically by react router
         // so we can use it to style the active menu item
         [`&.active`]: {
-         
-        },
-        ["&:hover"]: {
-          backgroundColor: "rgb(39, 44, 52)", // Hover background
-          color: "white", // Hover text color
-          border: "rgb(39, 44, 52)"
+          backgroundColor: '#003071',
+          color: '#b6c8d9',
         },
       },
     }}>
@@ -120,11 +93,9 @@ const ThisProSidebar = () => {
           <Item 
               title= {"Dashboard"}
               to="../"
-              icon = {<AppstoreOutlined />}
+              icon = {<HomeOutlinedIcon />}
               selected= {selected}
               setSelected= {setSelected} 
-
-             
           />
           <Item
               title={"Profile"}
@@ -132,7 +103,6 @@ const ThisProSidebar = () => {
               icon ={<PersonOutlinedIcon />}
               selected={selected}
               setSelected={setSelected}
-             
           />
           <Item 
               title={"Shared Data"}
@@ -140,7 +110,6 @@ const ThisProSidebar = () => {
               icon ={<ChatBubbleOutlineOutlinedIcon />}
               selected={selected}
               setSelected={setSelected}
-             
           />
           <Item 
               title={"Connections"}
@@ -148,7 +117,6 @@ const ThisProSidebar = () => {
               icon ={<LanguageIcon />}
               selected={selected}
               setSelected={setSelected}
-             
           />
           <Item 
               title={"My Insights"}
@@ -156,7 +124,6 @@ const ThisProSidebar = () => {
               icon ={<DocumentScannerOutlinedIcon />}
               selected={selected}
               setSelected={setSelected}
-
           />
           <Item 
               title={"Help / About"}
@@ -165,7 +132,6 @@ const ThisProSidebar = () => {
               icon ={<QuestionMarkOutlinedIcon />} 
               selected={selected}
               setSelected={setSelected}
-              
           />
           
           {/* <Item 
@@ -181,7 +147,6 @@ const ThisProSidebar = () => {
         </Box>
       </Menu>
     </Sidebar>
-    </div>
   );
 };
 
